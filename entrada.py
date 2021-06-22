db337ee86c78d207b45ce0a5d72875568ac92c78
SauloLSilva <86324559+SauloLSilva@users.noreply.github.com>
2021-06-22 17:39:17 -0300

    Estacionamento

---
+++ entrada.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+class Estacionamento:
+
+    def __init__(self):
+        self.__vagasTotais = 100
+        self.__vagasOcupadas = 0
+        self.__data = datetime.now().strftime('%H:%M dia:%d/%m/%Y')
+
+    def maximo(self):
+        return self.__vagasTotais
+
+    def disponiveis(self):
+        return self.__vagasTotais - self.__vagasOcupadas
+
+    def ocupadas(self):
+        return self.__vagasOcupadas
+
+    def entrada(self, placa):
+        if(self.__vagasOcupadas == self.__vagasTotais):
+            raise Exception('Estacionamento cheio!')
+        self.__vagasOcupadas += 1
+        entradaCarro = (placa, self.__data)
+        print('Entrada: {}'.format(entradaCarro))
+        with open('controle.txt', 'a') as arquivo:
+            for linha in entradaCarro:
+                arquivo.write(str('Entrada: {}\n'.format(entradaCarro)))
+
+    def saida( self,placa):
+        if( self.__vagasOcupadas == 0 ):
+            raise Exception('Estacionamento vazio!')
+        self.__vagasOcupadas -= 1
+        saidaCarro = (placa, self.__data)
+        print('Saida: {}'.format(saidaCarro))
+        with open('controle.txt', 'a') as arquivo:
+            for linha in saidaCarro:
+                arquivo.write(str('Saída: {}\n'.format(saidaCarro)))
+
