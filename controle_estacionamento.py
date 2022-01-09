b4690ece79499fcdb4311ff78b0988cdc19ba9cb
SauloLSilva <saulodasilva11@gamil.com>
2022-01-09 17:37:30 -0300

    Correcao de data fixa

---
+++ controle_estacionamento.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+class Estacionamento:
+
+    def __init__(self):
+        self.__vagasTotais = 100
+        self.__vagasOcupadas = 0
+
+    def horario(self):
+        data = datetime.now().strftime('%H:%M dia:%d/%m/%Y')
+        return data
+
+    def Cadastrar(self, pessoa, placa):
+            cadastroCarro = (pessoa, placa)
+            with open('cadastro.txt', 'a') as arquivo:
+                arquivo.write(str('Cadastro: Nome e Placa: {}-{} as {}\n'.format(cadastroCarro[0], cadastroCarro[1], self.horario())))
+            print('Cadastro: {} as {}'.format(cadastroCarro, self.horario()))
+
+    def entrada(self, placa):
+        if(self.__vagasOcupadas == self.__vagasTotais):
+            print('Estacionamento cheio!')
+        else:
+            self.__vagasOcupadas += 1
+            entradaCarro = (placa, self.horario())
+            print('Entrada: {}'.format(entradaCarro))
+            with open('controle.txt', 'a') as arquivo:
+                arquivo.write(str('Entrada: {} data: {}\n'.format(entradaCarro[0], entradaCarro[1])))
+
+    def saida( self,placa):
+        if( self.__vagasOcupadas == 0 ):
+            print('Estacionamento vazio!')
+        else:
+            self.__vagasOcupadas -= 1
+            saidaCarro = (placa, self.horario())
+            print('Saida: {}'.format(saidaCarro))
+            with open('controle.txt', 'a') as arquivo:
+                arquivo.write(str('Saída: {} data: {}\n'.format(saidaCarro[0], saidaCarro[1])))
+                arquivo.close()
+
+    def maximo(self):
+        return self.__vagasTotais
+
+    def disponiveis(self):
+        return self.__vagasTotais - self.__vagasOcupadas
+
+    def ocupadas(self):
+        return self.__vagasOcupadas
+    @property
+    def vagas(self):
+        print('Vagas ocupadas: {}'.format(self.__vagasOcupadas))
+        print('Vagas totais: {}'.format(self.__vagasTotais))
+        print('Data da consulta: {}\n'.format(self.horario()))
+
+
+
--- estacionamento.py
+++ estacionamento.py
@@ -1,51 +1,28 @@
-from datetime import datetime
-class Estacionamento:
+from controle_estacionamento import Estacionamento
+print('Bem vindo usuário')
+print('Digite opção para iniciar: ''\n')
+print('[1]Cadastro de carros\n[2]Entrada\n[3]Saida\n[4]Estado estacionamento\n')
+inicio = int(input('Digite valor para iniciar: '))
 
-    def __init__(self):
-        self.__vagasTotais = 100
-        self.__vagasOcupadas = 0
-        self.__data = datetime.now().strftime('%H:%M dia:%d/%m/%Y')
+e = Estacionamento()
+while (True):
+    if 1 == inicio:
+        nome = input('Digite nome usuário: ')
+        placa = input('Digite placa: ')
+        e.Cadastrar(nome, placa)
+    elif 2 == inicio:
+        placa = input('Digite placa: ')
+        e.entrada(placa)
+    elif 3 == inicio:
+        placa = input('Digite placa: ')
+        e.saida(placa)
+    elif 4 == inicio:
+        print('Informações:\n')
+        e.vagas
+    else:
+        print('Opção indisponível')
 
-    def Cadastrar(self, pessoa, placa):
-            cadastroCarro = (pessoa, placa)
-            with open('cadastro.txt', 'a') as arquivo:
-                arquivo.write(str('Cadastro: Nome e Placa: {}-{} as {}\n'.format(cadastroCarro[0], cadastroCarro[1], self.__data)))
-            print('Cadastro: {} as {}'.format(cadastroCarro, self.__data))
-
-    def entrada(self, placa):
-        if(self.__vagasOcupadas == self.__vagasTotais):
-            print('Estacionamento cheio!')
-        else:
-            self.__vagasOcupadas += 1
-            entradaCarro = (placa, self.__data)
-            print('Entrada: {}'.format(entradaCarro))
-            with open('controle.txt', 'a') as arquivo:
-                arquivo.write(str('Entrada: {} data: {}\n'.format(entradaCarro[0], entradaCarro[1])))
-
-    def saida( self,placa):
-        if( self.__vagasOcupadas == 0 ):
-            print('Estacionamento vazio!')
-        else:
-            self.__vagasOcupadas -= 1
-            saidaCarro = (placa, self.__data)
-            print('Saida: {}'.format(saidaCarro))
-            with open('controle.txt', 'a') as arquivo:
-                arquivo.write(str('Saída: {} data: {}\n'.format(saidaCarro[0], saidaCarro[1])))
-                arquivo.close()
-
-    def maximo(self):
-        return self.__vagasTotais
-
-    def disponiveis(self):
-        return self.__vagasTotais - self.__vagasOcupadas
-
-    def ocupadas(self):
-        return self.__vagasOcupadas
-    @property
-    def vagas(self):
-        print('Vagas ocupadas: {}'.format(self.__vagasOcupadas))
-        print('Vagas totais: {}'.format(self.__vagasTotais))
-        print('Data da consulta: {}\n'.format(self.__data))
-
+    print('[1]Cadastro de carros\n[2]Entrada\n[3]Saida\n[4]Estado estacionamento\n')
+    inicio = int(input('Digite valor para iniciar: '))
 
 
